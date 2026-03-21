3c22eed1e2ce532124dc941bec9f1d3c4241f9e4
Krrish.exe <krrishkoulia@gmail.com>
2026-03-22 00:30:12 +0530

    Refactor inventory model parameters and formatting

--- SCM_Project/inventory/inventory_models.py
+++ SCM_Project/inventory/inventory_models.py
@@ -1,11 +1,11 @@
-from typing import Optional , Literal
-from pydantic import BaseModel ,Field
+from pydantic import BaseModel, Field
 
 class InventoryParams(BaseModel):
-    """Basic parameter for inventory operations."""
-    D: float = Field(2000, gt = 0, description = 'Annual Demand (units per year)')
-    T_total: int = Field(365, ge = 1,description = 'Data in the simulation horizon')
-    LD: int = Field(0,ge = 0,description = 'Lead Time (days))')
-    T: int = Field(10,ge = 0,description = 'Review Perriod (days))')
-    Q: int = Field(0,ge = 0,description = 'Order Quanitity (units))')
-    initial_ioh: float = Field(0,ge = 0,description = 'Initial Inventory on Hand (units)')
+    """ Basic parameters for inventory operations. """
+    D: float = Field(2000, gt=0, description="Annual Demand (units per year)")
+    T_total: int = Field(365, ge=1, description="Days in the simulation horizon")
+    LD: int = Field(0, ge=0, description="Lead Time (days)")
+    T: int = Field(10, ge=1, description="Review Period (days)")
+    Q: float = Field(0, ge=0, description="Order Quantity (units)")
+    initial_ioh: float = Field(0, ge=0, description="Initial Inventory on Hand (units)")
+    sigma: float = Field(0, ge=0, description="Standard Deviation of Daily Demand (units)")
